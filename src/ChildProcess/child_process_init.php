444c4421787c6b0cd1b9e1f53a5afac1f211edcd
wpjscc <wpjscc@gmail.com>
2025-10-26 22:42:09 +0800

    chid process

--- src/ChildProcess/child_process_init.php
+++ src/ChildProcess/child_process_init.php
@@ -5,7 +5,7 @@ use React\Stream\WritableResourceStream;
 if (file_exists(__DIR__ . '/../../vendor/autoload.php')) {
     require __DIR__ . '/../../vendor/autoload.php';
 } else {
-    require __DIR__ . '/../../../vendor/autoload.php';
+    require __DIR__ . '/../../../../../vendor/autoload.php';
 }
 
 use ReactphpX\TunnelStream\TunnelStream;
