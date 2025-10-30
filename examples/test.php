31400a9e089de50d6e7c7981a8e36dbd16fa4847
wpjscc <wpjscc@gmail.com>
2025-10-30 08:52:48 +0800

    update

--- examples/test.php
+++ examples/test.php
@@ -15,19 +15,27 @@ $filesystem = Factory::createChildProcess();
 // memory leak when using Factory::create()
 // $filesystem = Factory::create();
 
+function getMemoryUsage(): array
+{
+    return [
+        'current_usage_mb' => round(memory_get_usage() / 1024 / 1024, 3),
+        'current_usage_real_mb' => round(memory_get_usage(true) / 1024 / 1024, 3),
+        'peak_usage_mb' => round(memory_get_peak_usage() / 1024 / 1024, 3),
+        'peak_usage_real_mb' => round(memory_get_peak_usage(true) / 1024 / 1024, 3),
+    ];
+}
+
 $file = $filesystem->file(__DIR__ . '/test.txt');
 async(function () use ($file) {
+    $startMemoryUsage = getMemoryUsage();
     while (true) {
         delay(1);
         $file->putContents(
             json_encode([
-                'current_usage_mb' => round(memory_get_usage() / 1024 / 1024, 3),
-                'current_usage_real_mb' => round(memory_get_usage(true) / 1024 / 1024, 3),
-                'peak_usage_mb' => round(memory_get_peak_usage() / 1024 / 1024, 3),
-                'peak_usage_real_mb' => round(memory_get_peak_usage(true) / 1024 / 1024, 3),
+                'start_memory_usage' => $startMemoryUsage,
+                'current_memory_usage' => getMemoryUsage(),
                 'memory_limit' => ini_get('memory_limit'),
             ], JSON_PRETTY_PRINT),
-            \FILE_APPEND
         )->then(function () {
             echo 'file written' . "\n";
         });
